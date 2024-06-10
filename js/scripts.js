4abb32ffbbdecd88c1f1252f6042597976339f55
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-06-10 16:38:50 +0900

    #3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // 애니메이션 및 기타 스크립트를 여기에 추가
 
 $(document).ready(function(){
+
     // Smooth scrolling for links
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
